4e3eed6a97ca6c9b95151d69fc942a4798d24a0f
ndizeyelesly1234 <139573491+Leslyndizeye@users.noreply.github.com>
2023-10-11 20:56:10 +0200

    main.sh

--- main.sh
+++ main.sh
@@ -104,18 +104,7 @@ function update_student() {
     clear
     ./main.sh
 }
-# New function to extract and sort email addresses
-function extract_and_sort_emails() {
-    # Extract email addresses from the student list and store them in a new file
-    grep -oE '[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:].]{2,4}' "$studentfile" > emails.txt
 
-    # Sort the email addresses and store them in a new file
-    sort -u -o sorted_emails.txt emails.txt
-
-    # Display the sorted email addresses
-    echo "Sorted Email Addresses:"
-    cat sorted_emails.txt
-}
 }
 #---------------------------------------Menu for program
 
