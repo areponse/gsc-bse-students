a0597e49ee682c7237b4e3e3f0b6472244bd217a
ndizeyelesly1234 <139573491+Leslyndizeye@users.noreply.github.com>
2023-10-13 15:20:02 +0200

    select-emails.sh

--- select-emails.sh
+++ select-emails.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-students_file="students-list_0923.txt"
-output_file="student-emails.txt"
+file_path='students-list_0923.txt'
+file_save='student-emails.txt'
 
-cut -d ',' -f 1 "$students_file" | sort > "$output_file"
+if [ -f "$file_path" ]; then
+    # Extract and sort emails in ascending order
+    awk -F '|' 'NR > 2 { gsub(/^[ \t]+|[ \t]+$/, "", $3); print $3 }' "$file_path" | sort > "$file_save"
+
+    # Loading message
+    echo -n "Opening Emails preview in ASC Order"
+    for i in {1..10}; do
+        echo -n "."
+        sleep 0.1
+    done
+    echo -e "100%\n"
+    # End of loading
+
+    ./main.sh
+else
+    echo "File doesn't exist"
+fi
