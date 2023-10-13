1d6fb5d76306c5e062aa36ae15a0eda2228c4036
lesly ndizeye <l.ndizeye@alustudent>
2023-10-13 05:48:32 -0700

    emails selected

--- select-emails.sh
+++ select-emails.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Input file containing student records
 students_file="students-list_0923.txt"
-
-# Output file to save student emails
 output_file="student-emails.txt"
 
-# Use grep to extract student emails and save them to the output file
-grep -oE '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b' "$students_file" > "$output_file"
-
-echo "Student emails extracted and saved to $output_file."
+cut -d ',' -f 1 "$students_file" | sort > "$output_file"
